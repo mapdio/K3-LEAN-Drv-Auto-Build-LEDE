836a50654775a31ef20c95d94ad8925a7e126077
mapdio <77219481+mapdio@users.noreply.github.com>
2021-02-07 23:36:38 +0800

    Update feed-clean.sh

--- feed-clean.sh
+++ feed-clean.sh
@@ -9,4 +9,4 @@ rm -rf feeds/kenzok8/redsocks2
 
 # 替换驱动
 
-mv -f $GITHUB_WORKSPACE/phicommk3-firmware-brcmfmac4366c-pcie.bin.69027 package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/brcmfmac4366c-pcie.bin
+# mv -f $GITHUB_WORKSPACE/phicommk3-firmware-brcmfmac4366c-pcie.bin.69027 package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/brcmfmac4366c-pcie.bin
